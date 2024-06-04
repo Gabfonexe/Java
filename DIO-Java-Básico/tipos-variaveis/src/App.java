14814d0607da6e0361abd0104201af71f3ee6f48
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-04 14:52:09 -0300

    att

--- DIO-Java-Básico/tipos-variaveis/src/App.java
+++ DIO-Java-Básico/tipos-variaveis/src/App.java
@@ -5,5 +5,13 @@ public class App {
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal; //usando o casting 
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
