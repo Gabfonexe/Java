2f0e51bb79de5f7261e7aa3a5b414bdf7d610020
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-04 15:19:33 -0300

    att

--- DIO-Java-Básico/tipos-variaveis/src/TiposVariaveis.java
+++ DIO-Java-Básico/tipos-variaveis/src/TiposVariaveis.java
@@ -1,10 +1,10 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500;
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal; //usando o casting 
+        short numeroCurto2 = (short) numeroNormal; // usando o casting
 
         int numero = 5;
 
