c0cd46995b6f5836b5db7799b5d058bbe27ed8ff
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-06 12:25:39 -0300

    update do For

---
+++ DIO-Java-Básico/controle-fluxo/src/ForEmArrays.java
@@ -0,0 +1,12 @@
+public class ForEmArrays {
+
+  public static void main(String[] args) {
+    
+    String alunos [] = {"MARCOS", "RAYANE", "LARA", "CLARA"};
+
+    for (int i=0; i< alunos.length; i++){
+      System.out.println("O aluno no indice i = " + i + " é " + alunos[i]);
+    }
+  }
+  
+}
