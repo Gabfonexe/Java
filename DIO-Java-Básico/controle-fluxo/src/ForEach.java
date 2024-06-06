42cef916d34540279d47905837edf6b1a80421a3
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-06 12:28:44 -0300

    update do For

---
+++ DIO-Java-Básico/controle-fluxo/src/ForEach.java
@@ -0,0 +1,11 @@
+public class ForEach {
+  
+  public static void main(String[] args) {
+    
+    String alunos [] = {"MARCOS", "RAYANE", "LARA", "CLARA"};
+    
+    for (String aluno : alunos){
+      System.out.println(aluno);
+    }
+  }
+}
