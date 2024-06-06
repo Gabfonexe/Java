e89b057b322a5a9af44c41d23b1c6b2daa8a2369
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-06 12:08:24 -0300

    atualizando repo

---
+++ DIO-Java-Básico/controle-fluxo/src/SwitchCaseSemBreak.java
@@ -0,0 +1,23 @@
+public class SwitchCaseSemBreak {
+
+  public static void main(String[] args) {
+		String plano = "M"; // M / T
+
+		switch (plano) {
+			case "T": {
+				System.out.println("5Gb Youtube");
+			}
+			case "M": {
+				System.out.println("Whats e Intagram grátis");
+			}
+			case "B": {
+				System.out.println("100 minutos de ligação");
+			}
+		}
+	}
+
+  
+  /*Nesse código, a utilização do switch case como condicional, favorece a simplicidade do código
+   * e contribui para as estruturas do código limpo
+   */
+}
