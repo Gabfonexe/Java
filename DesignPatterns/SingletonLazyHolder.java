8f31440c800aa745d077560abd1a31976fd0ddf0
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-21 00:08:41 -0300

    Singleton Lazy Holder

--- DesignPatterns/SingletonLazyHolder.java
+++ DesignPatterns/SingletonLazyHolder.java
@@ -1,5 +1,24 @@
 package DesignPatterns;
 
+/**
+ * Singleton "Lazy Holder"
+ * @see <a href = "https://stackoverflow.com/a/24018148">Referencia </a>
+ * 
+ * @author falvojr
+ */
+
+
 public class SingletonLazyHolder {
-  
+
+  private static class InstanceHolder{
+    public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+  }
+
+  private SingletonLazyHolder(){
+    super(); // não precisa colocar o super
+  }
+
+  public static SingletonLazyHolder getInstancia(){
+    return InstanceHolder.instancia;
+  }
 }
