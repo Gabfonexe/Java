8ed2f5be72e55a72fce7d43905b7ab6fb51ad41f
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-21 00:12:35 -0300

    main

---
+++ DesignPatterns/Test.java
@@ -0,0 +1,14 @@
+package DesignPatterns;
+
+public class Test {
+
+  public static void main(String[] args) {
+    SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+    System.out.println(lazyHolder);
+    lazyHolder = SingletonLazyHolder.getInstancia();
+    System.out.println(lazyHolder); 
+  }
+
+
+  
+}
