6a114158d735121a78be12913c032746e5c8fcf2
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-21 12:28:39 -0300

    Facade

--- DesignPatterns/Facade.java
+++ DesignPatterns/Facade.java
@@ -6,7 +6,7 @@ public class Facade {
     String cidade = CepApi.getInstancia().recuperarCidade(cep);
     String estado = CepApi.getInstancia().recuperarEstado(cep);
 
-    
+    CrmService.gravarCliente(nome, cep, cidade, estado);
   }
   
 }
