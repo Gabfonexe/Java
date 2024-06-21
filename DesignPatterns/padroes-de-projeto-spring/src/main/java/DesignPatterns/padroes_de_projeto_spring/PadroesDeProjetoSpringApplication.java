504adde2edea868f816f4f4048e4cf92e4fc8b49
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-21 12:46:29 -0300

    adicionando projeto

---
+++ DesignPatterns/padroes-de-projeto-spring/src/main/java/DesignPatterns/padroes_de_projeto_spring/PadroesDeProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package DesignPatterns.padroes_de_projeto_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesDeProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesDeProjetoSpringApplication.class, args);
+	}
+
+}
