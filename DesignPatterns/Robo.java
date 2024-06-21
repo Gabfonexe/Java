9172d66c7171d7bf9c3bee24b5c184a82911778e
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-21 11:50:44 -0300

    Strategy

---
+++ DesignPatterns/Robo.java
@@ -0,0 +1,14 @@
+package DesignPatterns;
+
+public class Robo {
+
+  private Comportamento comportamento; //isso é a strategy
+  
+  public void setComportamento(Comportamento comportamento){
+    this.comportamento = comportamento;
+  }
+
+  public void mover(){
+    comportamento.mover();
+  }
+}
--- DesignPatterns/Test.java
+++ DesignPatterns/Test.java
@@ -3,9 +3,27 @@ package DesignPatterns;
 public class Test {
 
   public static void main(String[] args) {
+
+    // Singleton
+
     SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
     System.out.println(lazyHolder);
     lazyHolder = SingletonLazyHolder.getInstancia();
     System.out.println(lazyHolder); 
+
+    // Strategy
+
+    Comportamento defensivo = new ComportamentoDefensivo();
+    Comportamento normal = new ComportamentoNormal();
+    Comportamento agressivo = new ComportamentoAgressivo();
+
+    Robo robo = new Robo();
+    robo.setComportamento(normal);
+    robo.mover();
+    robo.mover();
+    robo.setComportamento(defensivo);
+    robo.mover();
+    robo.setComportamento(agressivo);
+    robo.mover();
   }
 }
